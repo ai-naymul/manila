049faaea8668407ff7943f12177b2e10c6d3f843
Naymul Islam <naymul504@gmail.com>
2024-03-18 21:57:10 +0600

    refactor the lamda function and remove noqa comments

--- manila/test.py
+++ manila/test.py
@@ -444,11 +444,12 @@ class TestCase(base_test.BaseTestCase):
                 if k not in ignored_keys}
 
     def _assertEqualListsOfObjects(self, objs1, objs2, ignored_keys=None):
-        obj_to_dict = lambda o: (  # noqa: E731
-            self._dict_from_object(o, ignored_keys))
-        sort_key = lambda d: [d[k] for k in sorted(d)]  # noqa: E731
-        conv_and_sort = lambda obj: (  # noqa: E731
-            sorted(map(obj_to_dict, obj), key=sort_key))
+        def obj_to_dict(o):
+            return self._dict_from_object(o, ignored_keys)
+        def sort_key(d):
+            return [d[k] for k in sorted(d)]
+        def conv_and_sort(obj):
+            return sorted(map(obj_to_dict, obj), key=sort_key)
 
         self.assertEqual(conv_and_sort(objs1), conv_and_sort(objs2))
 
